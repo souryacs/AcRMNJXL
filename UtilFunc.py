7cb1116c5493c60dbf012d0ffce8603d615c4020
Sourya Bhattacharyya <souryacs@users.noreply.github.com>
2016-05-01 12:49:31 +0530

    Update UtilFunc.py

--- UtilFunc.py
+++ UtilFunc.py
@@ -21,3 +21,39 @@ def Read_Input_Treelist(ROOTED_TREE, PRESERVE_UNDERSCORE, INPUT_FILE_FORMAT, INP
 def Node_Label(inp_node):
 	return str(inp_node.as_newick_string(suppress_edge_lengths=True))
 
+##--------------------------------------------------
+#"""
+#this function computes the entropy of a numpy array input
+#"""
+#def Compute_Entropy(labels, outfile):
+	#if 1:	#(DEBUG_LEVEL >= 2):
+		#fp = open(outfile, 'a')
+	
+	#""" 
+	#Computes entropy of label distribution. 
+	#"""
+	#n_labels = len(labels)
+
+	#if n_labels <= 1:
+			#return 0
+
+	#counts = numpy.bincount(labels)
+	
+	#if 1:	#(DEBUG_LEVEL >= 2):
+		#fp.write('\n After bincount:   counts: ' + str(counts))
+	
+	#ent = 0
+	
+	#for i in range(len(counts)):
+		#if (counts[i] > 0):
+			#count_frac = ((counts[i] * 1.0) / n_labels)
+			#log_count_frac = math.log(count_frac, 2)
+			#ent = ent - (count_frac * log_count_frac)
+			#if 1:	#(DEBUG_LEVEL >= 2):
+				#fp.write('\n Nonzero count index: ' + str(i) + ' count_frac: ' + str(count_frac) + ' log_count_frac: ' + str(log_count_frac))
+			
+	#if 1:	#(DEBUG_LEVEL >= 2):
+		#fp.close()
+
+	#return ent
+
